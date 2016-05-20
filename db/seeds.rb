27c515eb8a34b66532c3e4186cd4c7d37f09bc8f
Jeshio <jeshio@yandex.ru>
2016-05-20 14:24:16 +0500

    Добавлено определение списка городов для db:seed из Factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_girl_rails'
+
+FactoryGirl.create_list(:country_with_cities, 5)
